44c9d88881115e21e2de798f64fc38c062aeb975
Nathan Webb <nwrocketman@byui.edu>
2021-05-17 21:59:34 -0600

    Completed Week 5 Reading.

---
+++ week5-part1/controllers/auth.js
@@ -0,0 +1,26 @@
+const User = require('../models/user');
+
+exports.getLogin = (req, res, next) => {
+  res.render('auth/login', {
+    path: '/login',
+    pageTitle: 'Login',
+    isAuthenticated: req.isLoggedIn
+  });
+};
+
+exports.postLogin = (req, res, next) => {
+  User.findOne()
+    .then(user => {
+      req.session.isLoggedIn = true;
+      req.session.user = user;
+      res.redirect('/');
+    })
+    .catch(err => console.log(err));
+};
+
+exports.postLogout = (req, res, next) => {
+  req.session.destroy(err => {
+    console.log(err);
+    res.redirect('/');
+  });
+};
